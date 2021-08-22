44b1b930f8ebcb86250a01797cad1dbdcf9f5b5a
lee-tae-eon <mktears0609@gmail.com>
2021-08-22 22:54:19 +0900

    testing with jest to movies.service

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -9,6 +9,7 @@ import {
   Query,
 } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
@@ -28,7 +29,6 @@ export class MoviesController {
 
   @Get(':id')
   getOne(@Param('id') movieId: number): Movie {
-    console.log(typeof movieId);
     return this.moviesService.getOne(movieId);
   }
 
@@ -43,7 +43,7 @@ export class MoviesController {
   }
 
   @Patch(`:id`)
-  patch(@Param(`id`) movieId: number, @Body() updateData) {
+  patch(@Param(`id`) movieId: number, @Body() updateData: UpdateMovieDto) {
     return this.moviesService.updateMovie(movieId, updateData);
   }
 }
--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
@@ -14,5 +15,82 @@ describe('MoviesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAll', () => {
+    it('shoud return an array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne', () => {
+    it('shoud return an movie', () => {
+      service.createMovie({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 1985,
+      });
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    });
+    it('shoud throw 404 error', () => {
+      try {
+        service.getOne(99);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual(`movie with Id 99 not found`);
+      }
+    });
+  });
+
+  describe('deleteOne', () => {
+    it('delete a movie', () => {
+      service.createMovie({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 1985,
+      });
+      const allMovies = service.getAll();
+      service.deleteOne(1);
+      const afterDelete = service.getAll();
+      expect(afterDelete.length).toEqual(allMovies.length - 1);
+    });
+    it('should return a 404', () => {
+      try {
+        service.deleteOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  describe('create', () => {
+    it('should create a movie', () => {
+      const beforeCreate = service.getAll().length;
+      service.createMovie({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 1985,
+      });
+      const afterCreate = service.getAll().length;
+      expect(afterCreate).toEqual(beforeCreate + 1);
+    });
+  });
+
+  describe('update', () => {
+    it('업데이트 무비', () => {
+      service.createMovie({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 1985,
+      });
+      service.updateMovie(1, {
+        title: 'updated test',
+      });
+      const movie = service.getOne(1);
+      expect(movie.title).toEqual('updated test');
+    });
   });
 });
